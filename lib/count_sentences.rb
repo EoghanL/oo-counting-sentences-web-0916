bf41cfc5bebe193ebe332985757f9a5270ca568e
github username <github email address>
2016-09-29 14:55:58 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
 require 'pry'
-
+stringer = "Hey, man. How are you today?"
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]/).delete_if { |sentence| sentence == "" }.length
   end
 end
